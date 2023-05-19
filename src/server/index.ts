b668aa6eeb983513358285479e86a1b784dd23ab
devferx <ferkki@hotmail.com>
2023-05-19 09:56:32 -0400

    Tu primer servidor con Express.js y TypeScript

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hola mundo desde el server");
+import express from 'express';
+import { config } from './config';
+
+const app = express();
+
+app.get("*", (req, res) => {
+  res.send(`<h1>Hola mundo con ruta: ${req.url}</h1>`);
+})
+
+app.listen(config.PORT, () => {
+  console.log(`Listening in http://localhost:${config.PORT}`);
+})
